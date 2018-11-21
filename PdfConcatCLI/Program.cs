434c8d92bb212d2e68807d7e8203dc4a72f05857
Jonas Kohl <github@jonaskohl.de>
2018-11-21 11:37:53 +0100

    Version 1.1.0.0

---
+++ PdfConcatCLI/Program.cs
@@ -0,0 +1,83 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace PdfConcatCLI
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                Console.WriteLine("PdfConcat Version {0}", version);
+                Console.WriteLine("Usage: PdfConcatCLI <inputFile1> <inputFile2> <inputFile3> ... <outputFile>");
+                Console.WriteLine("OR");
+                Console.WriteLine("Usage: PdfConcatCLI -w <inputWildcard> <outputFile>");
+                return 1;
+            }
+
+            string[] files;
+
+            if (
+                args.Length == 3
+                &&
+                args[0] == "-w"
+            )
+            {
+                string currentDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                files = Directory.GetFiles(currentDirectory, args[1], SearchOption.TopDirectoryOnly);
+            }
+            else
+                files = args.Take(args.Length - 1).ToArray();
+
+            string outputFiles = args.Last();
+
+            DoMerge(files, outputFiles);
+
+            return 0;
+        }
+
+        static void CopyPages(PdfDocument from, PdfDocument to)
+        {
+            for (int i = 0; i < from.PageCount; i++)
+                to.AddPage(from.Pages[i]);
+        }
+
+        static void DoMerge(string[] files, string outputFileName)
+        {
+            var docs = new List<PdfDocument>();
+            var outputDoc = new PdfDocument();
+
+            foreach (string file in files)
+                docs.Add(PdfReader.Open(file, PdfDocumentOpenMode.Import));
+
+            Console.WriteLine($"Total files: {files.Length}");
+
+            int progress = 1;
+
+            foreach (var doc in docs)
+            {
+                CopyPages(doc, outputDoc);
+                doc.Dispose();
+                Console.WriteLine("Processing file {0} of {1}: {2}", progress, files.Length, files[progress - 1]);
+                progress++;
+            }
+
+            outputDoc.Save(outputFileName);
+            outputDoc.Dispose();
+
+            docs.Clear();
+            docs = null;
+            GC.Collect();
+
+            Console.WriteLine($"Successfully merged {files.Length} PDF files to {outputFileName}!");
+        }
+    }
+}
---
+++ PdfConcatCLI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("PdfConcatCLI")]
+[assembly: AssemblyDescription("Concats multiple PDF documents.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jonas Kohl")]
+[assembly: AssemblyProduct("PdfConcatCLI")]
+[assembly: AssemblyCopyright("Copyright ©  2018 Jonas Kohl")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("332e20dc-eb0d-4ce5-b190-64a48a65803d")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
